76afb9413c7e126e252567a7af24651a86fb3fdd
jzinferno <jzinferno@gmail.com>
2023-07-27 12:33:06 +0300

    app: version '1.4'

--- app/src/main/java/com/jzinferno/swapmanager/JournalActivity.kt
+++ app/src/main/java/com/jzinferno/swapmanager/JournalActivity.kt
@@ -4,7 +4,6 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
 import java.io.File
